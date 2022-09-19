9f702d0c0d145b30ed6c87781f2b4b9de9c2fdc2
smgib <smgibb@yahoo.com>
2022-09-19 23:12:04 +0100

    Adding pin definitions

---
+++ Firmware/Pins.h
@@ -0,0 +1,12 @@
+#define CONN_1_G PB1
+#define CONN_1_R PB0
+#define CONN_1_B PA7
+
+#define CONN_2_G PA3
+#define CONN_2_R PA2
+#define CONN_2_B PA1
+
+#define CONN_3_G PB6
+#define CONN_3_R PB7
+#define CONN_3_B PB8
+
