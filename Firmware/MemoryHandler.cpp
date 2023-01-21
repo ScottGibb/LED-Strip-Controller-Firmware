3ee55a8412d53da0d995dc6e426284f22534a532
Scott Gibb <Kyle.Lawson@babcockdev.onmicrosoft.com>
2023-01-21 20:52:11 +0000

    Adding Memory Handler Implementation

--- Firmware/MemoryHandler.cpp
+++ Firmware/MemoryHandler.cpp
@@ -2,19 +2,68 @@
 
 #include <EEPROM.h>
 #include <stdbool.h>
+#include <map>
 
-
-MemoryHandler::MemoryHandler(){
-
-}
-MemoryHandler::~MemoryHandler(){
-  
-}
-bool MemoryHandler::saveData(Segment seg, uint8_t pos, uint8_t *data, uint8_t dataLen) {
-    return false;
-}
-
-bool MemoryHandler::loadData(Segment seg, uint8_t pos, uint8_t *data, uint8_t dataLen)
+MemoryHandler::MemoryHandler(std::map<SEGMENT, MemoryMap_t> memory)
+    : MEMORY_MAP(memory)
 {
-  return false;
+}
+MemoryHandler::~MemoryHandler()
+{
+}
+MEMORY_ERR MemoryHandler::saveData(SEGMENT seg, uint16_t pos, uint8_t *data, uint16_t dataLen)
+{
+  uint16_t saveAddress = MEMORY_MAP[seg].MEMORY_START + MEMORY_MAP[seg].SLOT_SIZE * pos;
+  MEMORY_ERR error = checkValidity();
+  if (error == MEMORY_ERR::OK)
+  {
+    for (uint16_t i = 0; i < dataLen; i++)
+    {
+      if (i > MEMORY_MAP[seg].MEMORY_END)
+      {
+        return MEMORY_ERR::SEGMENT_OUT_OF_BOUNDS;
+      }
+      EEPROM.update(saveAddress + i, data[i]);
+    }
+  }
+
+  return error;
+}
+
+MEMORY_ERR MemoryHandler::loadData(SEGMENT seg, uint16_t pos, uint8_t *data, uint16_t dataLen)
+{
+
+  uint16_t loadAddress = MEMORY_MAP[seg].MEMORY_START + MEMORY_MAP[seg].SLOT_SIZE * pos;
+  MEMORY_ERR error = checkValidity();
+
+  if(error == MEMORY_ERR::OK){
+    for(uint16_t i =0; i < dataLen; i++){
+      if(i > MEMORY_MAP[seg].MEMORY_END;i++){
+        return MEMORY_ERR::SEGMENT_OUT_OF_BOUNDS;
+      }
+      EEPROM.get(loadAddress+i, data[i]);
+    }
+  }
+}
+
+MEMORY_ERR MemoryHandler ::checkValidity(Segment seg, uint16_t pos, uint16_t dataLen)
+{
+  if (pos > MEMORY_MAP[seg].NUM_SLOTS)
+  {
+    return MEMORY_ERR::OUT_OF_SLOTS;
+  }
+  else if (dataLen > MEMORY_MAP[seg].SLOT_SIZE)
+  {
+    return MEMORY_ERR::SLOT_OUT_OF_BOUNDS;
+  }
+}
+
+MemoryHandler MemoryHandler::getInstance(std::map<SEGMENT, MemoryMap_t> memory)
+{
+  if(memoryHandler == nullptr){
+      memoryHandler = new MemoryHandler(memory);
+      return memoryHandler;
+  }else{
+    return memoryHandler
+  }
 }
--- Firmware/MemoryHandler.h
+++ Firmware/MemoryHandler.h
@@ -1,24 +1,50 @@
 #ifndef __STATE_SAVER_H__
 #define __STATE_SAVER_H__
 
-#include "STM32F103C8T6MemoryMap.h"
 #include <stdint.h>
+#include <map>
 
-enum class Segment{
+enum class SEGMENT
+{
     SYSTEM_INFO = 0,
-    CHANNEL_CMDS =1,
-    USER_MODES =2
+    CHANNEL_CMDS = 1,
+    USER_MODES = 2,
+    NUM_MEMORY_SEGMENTS =3
+
 
 };
 
-class MemoryHandler{
+enum class MEMORY_ERR{
+    OK =0,
+    SEGMENT_OUT_OF_BOUNDS =1,
+    OUT_OF_SLOTS =2,
+    SLOT_OUT_OF_BOUNDS =3
+}
+
+using MemoryMap_t = struct MemoryMap
+{
+    uint16_t MEMORY_START;
+    uint16_t MEMORY_END;
+    uint16_t MEMORY_SIZE;
+    uint16_t SLOT_SIZE;
+    uint16_t NUM_SLOTS;
+};
+
+class MemoryHandler
+{
 public:
-    MemoryHandler();
-    ~MemoryHandler();
-    bool saveData(Segment seg, uint8_t pos,uint8_t *data, uint8_t dataLen);
-    bool loadData(Segment seg, uint8_t pos,uint8_t *data, uint8_t dataLen);
+   
+    MEMORY_ERR saveData(SEGMENT seg, uint16_t pos, uint8_t *data, uint16_t dataLen);
+    MEMORY_ERR loadData(SEGMENT seg, uint16_t pos, uint8_t *data, uint16_t dataLen);
+    static MemoryHandler * getInstance();
 
 private:
+    static MemoryHandler* memoryHandler =nullptr;
+    std::map<SEGMENT, MemoryMap_t> MEMORY_MAP;
+     MemoryHandler(std::map<SEGMENT, MemoryMap_t> MemoryMap);
+    ~MemoryHandler();
+    MEMORY_ERR MemoryHandler :: checkValidity(Segment seg, uint16_t pos, uint16_t dataLen);
+
 };
 
 #endif
